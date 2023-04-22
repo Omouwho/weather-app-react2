536e275779825220397ec8478421bf478b47aa89
Omouwho <esther.omouwho@gmail.com>
2023-04-22 07:31:48 +0100

    Added a footer

---
+++ src/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      This project is coded by
+      <a href="https://esthers-portfolio-project.netlify.app/">
+        {" "}
+        Esther Omouwho{" "}
+      </a>{" "}
+      it is open-sourced on{" "}
+      <a
+        href="https://github.com/Omouwho/weather-app-react2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>{" "}
+      and hosted on{" "}
+      <a
+        href="https://omouwhos-react-weather-app.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Netlify
+      </a>
+    </div>
+  );
+}
