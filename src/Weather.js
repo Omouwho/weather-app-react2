f58b54f18ea4d2e66d652531ba858c2ca2355e9f
Omouwho <esther.omouwho@gmail.com>
2023-04-22 11:18:40 +0100

    Added css styling

--- src/Weather.js
+++ src/Weather.js
@@ -10,10 +10,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -22,13 +27,19 @@ export default function Weather() {
         <li>Saturday 08:00</li>
         <li>Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="sunny"
-          />{" "}
-          33°C
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="sunny"
+              className="float-left"
+            />{" "}
+            <div className="float-left">
+              <span className="temperature"> 33</span>
+              <span className="unit"> °C</span>
+            </div>
+          </div>
         </div>
 
         <div className="col-6">
