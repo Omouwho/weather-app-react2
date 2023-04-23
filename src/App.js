5d0b844d6c6cc617f3d7a7a74f39baf7caa48c9b
Omouwho <esther.omouwho@gmail.com>
2023-04-23 07:57:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Sunderland" />
+        <Weather defaultCity="Lagos" />
         <Footer />
       </div>
     </div>
