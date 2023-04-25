82a6e558b52b0e5e877d24b959cc896c160c58c9
Omouwho <esther.omouwho@gmail.com>
2023-04-25 09:20:15 +0100

    css styling

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,70 +17,38 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
-  // console.log(props);
-  if (loaded) {
-    // console.log(forecast);
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-              // } else {
-              //   return null;
-            }
-          })}
-        </div>
-      </div>
-    );
-  } else {
+  function load() {
     let apiKey = "b1ffa750faa242739962f64fe0t9dod4";
     let longitude = props.coordinates.longitude;
     let latitude = props.coordinates.latitude;
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
+  }
+  // console.log(props);
+  if (loaded) {
+    // console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <hr />
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col-2" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
 
     return null;
   }
 }
-
-//
-
-//   function load() {
-//     let apiKey = "b1ffa750faa242739962f64fe0t9dod4";
-//     let longitude = props.coordinates.longitude;
-//     let latitude = props.coordinates.latitude;
-//     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
-
-//     axios.get(apiUrl).then(handleResponse);
-//   }
-
-//   if (loaded) {
-//     return (
-//       <div className="WeatherForecast">
-//         <div className="row">
-//           {forecast.map(function (dailyForecast, index) {
-//             if (index < 6) {
-//               return (
-//                 <div className="col-2" key={index}>
-//                   <WeatherForecastDay data={dailyForecast} />
-//                 </div>
-//               );
-//             } else {
-//               return null;
-//             }
-//           })}
-//         </div>
-//       </div>
-//     );
-//   } else {
-//     load();
-
-//     return null;
-//   }
-// }
